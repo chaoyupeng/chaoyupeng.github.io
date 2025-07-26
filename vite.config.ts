0ad2d824d6901f5bfed697ed91f5b082828aef47
Yupeng Chao <60679518+chaoyupeng@users.noreply.github.com>
2025-07-26 15:50:32 +1000

    Fix Tailwind CSS and MIME type issues

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,21 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   },
   esbuild: {
     target: 'es2020'
+  },
+  server: {
+    fs: {
+      strict: true
+    }
+  },
+  optimizeDeps: {
+    include: ['lit']
   }
 })
